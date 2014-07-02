fddde50da3bbbd119ecca2c80942ff78ef855bfe
Fabio Cigliano <fabio.cigliano@gmail.com>
2014-07-02 14:27:20 +0000

    added the PLIST parser PHP library

---
+++ plist-parser/plist_parser.php
@@ -0,0 +1,9 @@
+<?php
+
+if(!defined('LIBDIR')) {
+  define('LIBDIR',__DIR__);
+}
+
+require_once(LIBDIR.'/CFPropertyList.php');
+
+?>
---
+++ plist_parser.php
@@ -0,0 +1,9 @@
+<?php
+
+if(!defined('LIBDIR')) {
+  define('LIBDIR',__DIR__.'/'.'plist-parser');
+}
+
+require_once(LIBDIR.'/CFPropertyList.php');
+
+?>
